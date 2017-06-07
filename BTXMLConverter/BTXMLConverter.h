e1b765d3c0135181535b385e6307208b1b353919
BaoTuan <tuan.diep@dirox.net>
2017-06-08 01:01:17 +0700

    Initial Commit

---
+++ BTXMLConverter/BTXMLConverter.h
@@ -0,0 +1,19 @@
+//
+//  BTXMLConverter.h
+//  BTXMLConverter
+//
+//  Created by BaoTuan on 6/8/17.
+//  Copyright © 2017 Baotuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BTXMLConverter.
+FOUNDATION_EXPORT double BTXMLConverterVersionNumber;
+
+//! Project version string for BTXMLConverter.
+FOUNDATION_EXPORT const unsigned char BTXMLConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTXMLConverter/PublicHeader.h>
+
+
